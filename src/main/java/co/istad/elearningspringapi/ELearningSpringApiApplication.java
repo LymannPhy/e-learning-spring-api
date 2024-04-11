cd63851b508ae8b94ce27a6753501c79ad51f2ae
lymann <phplaravel576@gmail.com>
2024-04-11 12:29:40 +0700

    define project structure

---
+++ src/main/java/co/istad/elearningspringapi/ELearningSpringApiApplication.java
@@ -0,0 +1,13 @@
+package co.istad.elearningspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/istad/elearningspringapi/ELearningSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.elearningspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
