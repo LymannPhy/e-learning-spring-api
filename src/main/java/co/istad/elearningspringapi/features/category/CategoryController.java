b62e87bdbee31147911387925b80f7f0271075ee
MengseuThoeng <mengseu2004@gmail.com>
2024-04-13 11:25:03 +0700

    Init File

---
+++ src/main/java/co/istad/elearningspringapi/features/category/CategoryController.java
@@ -0,0 +1,14 @@
+package co.istad.elearningspringapi.features.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+}
